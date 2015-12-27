0f13bb36328b15b160dffd805db65a6b9364b540
kobi <nir.belinky@kaltura.com>
2015-12-27 18:43:08 +0200

    first list

--- plugin.video.kaltura/addon.py
+++ plugin.video.kaltura/addon.py
@@ -2,12 +2,28 @@ import sys
 import xbmcgui
 import xbmcplugin
 
-addon_handle = int(sys.argv[1])
+sys.path.append('/home/kobi/.kodi/addons/python/KalturaClient/')
+sys.path.append('/home/kobi/.kodi/addons/python/')
+
+
+
+from addOnSettings import *
+from GetBaseList import *
+from itemInfo import *
+
+addon_handle    = int(sys.argv[1])
+addon_settings  = addOnSettings()
+addon_ks        = addon_settings.getKS()
 
 xbmcplugin.setContent(addon_handle, 'movies')
+play_list = GetBaseList(addon_ks).getPartnerEntryList()
+info_list = ItemInfoList(play_list);
 
-url = 'http://cfvod.kaltura.com/pd/p/1875831/sp/187583100/serveFlavor/entryId/1_9aali829/v/1/flavorId/1_q9lyy0g5/fileName/Creating_together_(Source).mov/name/a.mov'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-
+for i in range(len(play_list)):
+    item_info = info_list.getItemInfo(i);
+    url = item_info.downloadUrl
+    li = xbmcgui.ListItem(item_info.name, iconImage='DefaultVideo.png')
+    li.setArt({'thumb': item_info.thumbUrl})
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=item_info.downloadUrl, listitem=li)
 xbmcplugin.endOfDirectory(addon_handle)
+
