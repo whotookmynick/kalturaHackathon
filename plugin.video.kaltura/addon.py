5154d46b36d1bfa903ac91155742e74d54dc44bc
Noam <noamaradjunk@gmail.com>
2015-12-28 11:38:39 +0200

    add fanart and fix settings

--- plugin.video.kaltura/addon.py
+++ plugin.video.kaltura/addon.py
@@ -1,6 +1,7 @@
 import sys
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 import pprint
 
 from addOnSettings  		import *
@@ -12,6 +13,7 @@ from kalturaStreamInfoAdapter   import *
 addon_handle        = int(sys.argv[1])
 addon_settings      = addOnSettings()
 kaltura_addon_ks    = addon_settings.getKS()
+kaltura_addon = xbmcaddon.Addon('plugin.video.kaltura')
 
 xbmcplugin.setContent(addon_handle, 'movies')
 kaltura_play_list   = GetBaseList(kaltura_addon_ks).getPartnerEntryList()
@@ -21,7 +23,7 @@ for i in range(len(kaltura_play_list)):
     item_info       = info_list.getItemInfo(i);
     url             = item_info.downloadUrl
     kodi_list_item  = xbmcgui.ListItem(item_info.name, iconImage='DefaultVideo.png')
-    kodi_list_item.setArt({'thumb': item_info.thumbUrl})
+    kodi_list_item.setArt({'thumb': item_info.thumbUrl, 'fanart':kaltura_addon.getAddonInfo('fanart')})
     kodi_item_info  = KalturaItemInfoAdapter(kaltura_play_list[i])
     kodi_item_info.setKodiItemInfo(kodi_list_item)
     kodi_stream_info = KalturaStreamInfoAdapter(kaltura_play_list[i])
