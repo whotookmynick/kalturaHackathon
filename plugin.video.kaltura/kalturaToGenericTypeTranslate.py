d41e27b9053b6b1a0674747c526212a6ea51ba15
kobimichaeli <kobi.michaeli@kaltura.com>
2015-12-28 15:35:16 +0200

    icon handling

---
+++ plugin.video.kaltura/kalturaToGenericTypeTranslate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+from iconMap import iconMap
+
+class kalturaToGenericTypeTranslate:
+	relevant_types = {'1' : 'clip', '2' : 'image', '5' : 'audio', '201' : 'live'}
+	relevant_types_keys = []
+	icon_mapping = ""
+
+	def __init__(self):
+		self.relevant_types_keys = self.relevant_types.keys()
+		self.icon_mapping = iconMap()
+
+	def mapKalturaType(self, type_id):
+		if (type_id in self.relevant_types_keys):
+			print "found "+self.relevant_types[type_id]
+			ret = self.icon_mapping.get_type_from_dict(self.relevant_types[type_id])
+			return ret
+		else:
+			print "not found"
+			return self.icon_mapping.get_type_from_dict('blank')
+
+	def getRelevant_types_keys(self):
+		return str(self.relevant_types_keys)
+
+# Main
+if __name__=="__main__":
+	type_map = kalturaToGenericTypeTranslate()
+	if len(sys.argv) < 2:
+	    print "No parameters were given. You must provide one of the following:."+ type_map.getRelevant_types_keys()
+	    exit (1)
+	print type_map.mapKalturaType(sys.argv[1])
+	exit (0) 
+ 
